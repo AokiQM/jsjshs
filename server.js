935236065cbb61529517f01b6aa9c397d812eacd
青木𝙰𝚘𝚔𝚒.𝚀𝙼 <3452798215@qq.com>
2026-02-15 23:24:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ app.post("/verify", async (req, res) => {
   return res.json({ success: true });
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+ console.log("Server running on port " + PORT);
 });
